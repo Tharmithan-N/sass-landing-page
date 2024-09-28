d0e32e99ba123e209e44e4fb58c0c8dc6d5a4ad5
Tharmithan <tharmithan@orelit.com>
2024-09-28 11:40:12 +0530

    Hero modification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TopNav from "./Components/TopNav/TopNav";
+import Hero from "./Components/Header/Hero/Hero";
 
 function App() {
-  return <div><TopNav/></div>;
+  return <div><TopNav/><Hero/></div>;
 }
 
 export default App;
